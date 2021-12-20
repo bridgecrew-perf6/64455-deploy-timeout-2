77c4cda8151f227607ffd2de14648553892e48b6
Fabien Franzen <info@atelierfabien.be>
2021-12-20 23:29:50 +0100

    Fix LoginModal

--- src/components/Common/Modal/Login.js
+++ src/components/Common/Modal/Login.js
@@ -1,17 +1,35 @@
+import { useMemo, useRef } from 'react';
+
+import { withPortal } from '@foundation/next';
+
 import AuthLogin from '@app/components/Auth/Login';
 
-const CommonModalLogin = () => (
-  <div id="login-modal" className="uk-modal-full uk-modal" uk-modal="true">
+const CommonModalLogin = withPortal(() => {
+  const ref = useRef();
+
+  const onSubmit = useMemo(() => {
+    return () =>
+      ref.current && setTimeout(() => UIkit.modal(ref.current).hide(), 300);
+  }, [ref]);
+
+  return (
     <div
-      className="uk-modal-dialog uk-flex uk-flex-center uk-flex-middle"
-      uk-height-viewport="true"
+      id="login-modal"
+      ref={ref}
+      className="uk-modal-full uk-modal"
+      uk-modal="container: #__next"
     >
-      <button className="uk-modal-close-full" type="button" uk-close="true" />
-      <div className="uk-text-center">
-        <AuthLogin />
+      <div
+        className="uk-modal-dialog uk-flex uk-flex-center uk-flex-middle"
+        uk-height-viewport="true"
+      >
+        <button className="uk-modal-close-full" type="button" uk-close="true" />
+        <div className="uk-text-center">
+          <AuthLogin onSubmit={onSubmit} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+});
 
 export default CommonModalLogin;
