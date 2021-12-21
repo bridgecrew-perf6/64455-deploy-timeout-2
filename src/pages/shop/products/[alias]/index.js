85c2745900ce73eacc6185c9ec4cd70e062102b4
Fabien Franzen <info@atelierfabien.be>
2021-12-21 17:07:58 +0100

    Debug /shop/products/[alias] endpoint

--- src/pages/shop/products/[alias]/index.js
+++ src/pages/shop/products/[alias]/index.js
@@ -1,4 +1,8 @@
-import { Product, getStaticProps, getStaticPaths } from '@shop/http/products';
+import { getStaticProps, getStaticPaths } from '@shop/http/products';
+
+const Product = props => {
+  return <code>{JSON.stringify(props, null, 4)}</code>;
+};
 
 export default Product;
 export { getStaticProps, getStaticPaths };
