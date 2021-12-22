ce4ffd31e6b946294e49a462612034744dfbd98a
Fabien Franzen <info@atelierfabien.be>
2021-12-22 11:07:39 +0100

    Minimize Types/Product override

--- src/components/Page/Types/Product.js
+++ src/components/Page/Types/Product.js
@@ -1,19 +1,5 @@
-import { usePropsOrPage, get } from '@foundation/next';
-
-const ProductLayout = ({ page, children }) => {
-  const { layout } = usePropsOrPage(page);
-  const containerProps = get(layout, ['options', 'containerProps'], {});
-
-  return (
-    <section
-      data-part="layout"
-      data-page-type="basic"
-      {...containerProps}
-      uk-height-viewport="expand: true"
-    >
-      {children}
-    </section>
-  );
+const ProductLayout = ({ children }) => {
+  return <div>{children}</div>;
 };
 
 export default ProductLayout;
