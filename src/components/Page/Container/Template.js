7f59f16b68db001632a2f0a441cdf8e5083fcb24
Fabien Franzen <info@atelierfabien.be>
2021-12-22 12:10:39 +0100

    Remove useMemo

--- src/components/Page/Container/Template.js
+++ src/components/Page/Container/Template.js
@@ -1,15 +1,9 @@
-import { useMemo } from 'react';
-
-// import types from '@shop/components/Page/Types';
+import types from '@shop/components/Page/Types';
 
 const DefaultComponent = ({ children }) => <>{children}</>;
 
 const Template = ({ type, children, ...props }) => {
-  const Component = useMemo(() => {
-    return DefaultComponent;
-    // return types.get(type) ?? types.get('default');
-  }, [type]);
-
+  const Component = types.get(type) ?? types.get('default') ?? DefaultComponent;
   return <Component {...props}>{children}</Component>;
 };
 
