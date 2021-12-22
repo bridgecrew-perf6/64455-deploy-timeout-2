2e5c675d8d034720d631b0969b496e14f14700c0
Fabien Franzen <info@atelierfabien.be>
2021-12-22 14:22:11 +0100

    Try Types/Account workaround

--- src/components/Page/Container/Template.js
+++ src/components/Page/Container/Template.js
@@ -1,25 +1,23 @@
-// import { useMemo } from 'react';
+import { useMemo } from 'react';
 
-// import types from '@shop/components/Page/Types';
+import types from '@shop/components/Page/Types';
 
-const DefaultComponent = ({ children }) => (
-  <div className="uk-padding">{children}</div>
-);
+const DefaultComponent = ({ children }) => <>{children}</>;
 
-// const Template = ({ type, children, ...props }) => {
-//   const Component = useMemo(() => {
-//     return types.get(type) ?? types.get('default') ?? DefaultComponent;
-//   }, [type]);
-//   return <Component {...props}>{children}</Component>;
-// };
-
-const Template = ({ children }) => {
-  return (
-    <DefaultComponent>
-      <h1>Debugging</h1>
-      {children}
-    </DefaultComponent>
-  );
+const Template = ({ type, children, ...props }) => {
+  const Component = useMemo(() => {
+    return types.get(type) ?? types.get('default') ?? DefaultComponent;
+  }, [type]);
+  return <Component {...props}>{children}</Component>;
 };
+
+// const Template = ({ children }) => {
+//   return (
+//     <DefaultComponent>
+//       <h1>Debugging</h1>
+//       {children}
+//     </DefaultComponent>
+//   );
+// };
 
 export default Template;
--- src/components/Page/Types/Account.js
+++ src/components/Page/Types/Account.js
@@ -1,13 +1,18 @@
-import { withPageWithLayout } from '@shop/hooks';
+// import { withPageWithLayout } from '@shop/hooks';
 
 import AccountOverview from '@shop/components/Site/Account/Overview';
 
+import { usePage } from '@foundation/next';
+
 const AccountPage = props => {
+  const page = usePage(props);
   return (
     <div className="main-container">
-      <AccountOverview {...props} />
+      <AccountOverview {...props} page={page} />
     </div>
   );
 };
 
-export default withPageWithLayout('account', AccountPage);
+// export default withPageWithLayout('account', AccountPage);
+
+export default AccountPage;
