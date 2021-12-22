b12838a8d883f8599b2fca40821344c3c793c826
Fabien Franzen <info@atelierfabien.be>
2021-12-22 12:00:37 +0100

    Add Page/Container/Template component override

---
+++ src/components/Page/Container/Template.js
@@ -0,0 +1,16 @@
+import { useMemo } from 'react';
+
+import types from '@shop/components/Page/Types';
+
+const DefaultComponent = ({ children }) => <>{children}</>;
+
+const Template = ({ type, children, ...props }) => {
+  const Component = useMemo(() => {
+    if (type === 'product') return DefaultComponent;
+    return types.get(type) ?? types.get('default');
+  }, [type]);
+
+  return <Component {...props}>{children}</Component>;
+};
+
+export default Template;
