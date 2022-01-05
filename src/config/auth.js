c0bd37db3c5415c50a2ebbce71380216c2b15512
Fabien Franzen <info@atelierfabien.be>
2022-01-05 18:53:35 +0100

    Redirect to /account

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,9 +1,16 @@
 import configure from '@atelierfabien/next-auth/configure';
 
+import { joinUrl } from '@atelierfabien/next-foundation/lib/util';
+
 export default configure({
   referencedTypes: ['product', 'recipe', 'recipe.bundle'],
   signUp: {
     disabled: true,
     managed: true,
+  },
+  callbacks: {
+    async redirect(url, baseUrl) {
+      return joinUrl(baseUrl, '/account');
+    },
   },
 });
